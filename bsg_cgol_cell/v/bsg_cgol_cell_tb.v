2bcd9eab56bb535f351d58b5d51a2f807b497d05
Huwan Peng <hwpeng20@gmail.com>
2023-02-24 16:33:29 -0800

    Update the handshake logic in cell testbench.

--- bsg_cgol_cell/v/bsg_cgol_cell_tb.v
+++ bsg_cgol_cell/v/bsg_cgol_cell_tb.v
@@ -24,22 +24,20 @@ module bsg_cgol_cell_tb;
       ,.async_reset_o( reset )
       );
 
-  logic dut_v_r;
-  logic [9:0] dut_data_r;
 
   logic tr_v_lo;
   logic [9:0] tr_data_lo;
   logic tr_ready_lo;
+  logic tr_yumi_li;
+  logic tr_yumi_lo;
 
   logic [31:0] rom_addr_li;
   logic [13:0] rom_data_lo;
 
-  logic tr_yumi_li;
-
+  logic dut_v_r;
+  logic dut_data_lo;
   logic [7:0] data_li;
   logic en_li, update_li, update_val_li;
-  logic data_lo;
-
 
   bsg_fsb_node_trace_replay #(.ring_width_p(10)
                              ,.rom_addr_width_p(32) )
@@ -49,7 +47,7 @@ module bsg_cgol_cell_tb;
       , .en_i( 1'b1 )
 
       , .v_i    ( dut_v_r )
-      , .data_i ( dut_data_r )
+      , .data_i ( {9'b0, dut_data_lo} )
       , .ready_o( tr_ready_lo )
 
       , .v_o   ( tr_v_lo )
@@ -62,23 +60,6 @@ module bsg_cgol_cell_tb;
       , .done_o()
       , .error_o()
       );
-
-  assign tr_yumi_li = tr_v_lo;
-
-  logic tr_yumi_li_r;
-  always_ff @(negedge clk) begin
-    tr_yumi_li_r <= tr_yumi_li;
-    if (tr_v_lo) begin
-      en_li <= tr_data_lo[9];
-      update_li <= ~tr_data_lo[9];
-      update_val_li <= tr_data_lo[8];
-      data_li <= tr_data_lo[7:0];
-    end
-    else begin
-      en_li <= 0;
-      update_li <= 0;
-    end
-  end
 
   trace_rom #(.width_p(14),.addr_width_p(32))
     ROM
@@ -94,20 +75,32 @@ module bsg_cgol_cell_tb;
     ,.update_i     (     update_li )
     ,.update_val_i ( update_val_li )
 
-    ,.data_o       (       data_lo )
+    ,.data_o       (   dut_data_lo )
     );
 
-  always_ff @(negedge clk) begin
+  // input handshake for DUT, it can consume new data in each cycle
+  assign en_li = tr_v_lo & tr_data_lo[9];
+  assign update_li = tr_v_lo & (~tr_data_lo[9]);
+  assign update_val_li = tr_data_lo[8];
+  assign data_li = tr_data_lo[7:0];
+  assign tr_yumi_li = tr_v_lo; 
+
+  // output handshake for DUT, valid then yumi
+  always_ff @(posedge clk) begin
     if (reset) begin
       dut_v_r <= 0;
     end
-    else if (tr_yumi_li_r) begin
-      dut_data_r <= {9'b0, data_lo};
-      dut_v_r <= 1;
+    else begin
+      if (tr_v_lo)
+        dut_v_r <= 1;
+      else if (tr_yumi_lo)
+        dut_v_r <= 0;
     end
-    else if (tr_ready_lo & dut_v_r) begin
-      dut_v_r <= 0;
-    end
+  end
+  
+  // trace_replay yumi
+  always_ff @(negedge clk) begin
+    tr_yumi_lo <= tr_ready_lo & dut_v_r;
   end
 
 endmodule
